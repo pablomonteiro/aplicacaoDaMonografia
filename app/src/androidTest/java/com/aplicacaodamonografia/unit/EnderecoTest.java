faa28a2e8eb2f8dd43e2fb03961502175066d889
PabloMonteiro <pablomf84@gmail.com>
2016-02-21 22:56:43 -0300

    Adicionando configuracao das dependencias para realizacao dos testes. Comentando teste com Robolectric.

--- app/src/androidTest/java/com/aplicacaodamonografia/unit/EnderecoTest.java
+++ app/src/androidTest/java/com/aplicacaodamonografia/unit/EnderecoTest.java
@@ -1,22 +1,52 @@
 package com.aplicacaodamonografia.unit;
 
+import android.test.AndroidTestRunner;
 import android.test.suitebuilder.annotation.SmallTest;
 
-import org.junit.Assert;
+import com.aplicacaodamonografia.BuildConfig;
+import com.aplicacaodamonografia.exception.EnderecoLoadException;
+import com.aplicacaodamonografia.model.Endereco;
+
+import static org.junit.Assert.*;
+
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
-import org.robolectric.RobolectricGradleTestRunner;
+//import org.robolectric.RobolectricGradleTestRunner;
+//import org.robolectric.RobolectricTestRunner;
+//import org.robolectric.annotation.Config;
 
 /**
  * Created by pablo on 13/02/16.
  */
-@SmallTest
-@RunWith(RobolectricGradleTestRunner.class)
+
+//@RunWith(RobolectricTestRunner.class)
+//@Config(constants = BuildConfig.class)
 public class EnderecoTest {
 
-    @Test
-    public void test() {
-        Assert.assertTrue(true);
+//    @Rule
+    public ExpectedException thrown= ExpectedException.none();
+
+//    @Test
+//    @Config(manifest = Config.NONE)
+    public void deveriaConverterJsonParaEndereco() {
+        String jsonDoEndereco = "{\"uf\":\"PB\",\"cidade\":\"Patos\", \"bairro\":\"Centro\", \"logradouro\":\"Rua 1\"}";
+        Endereco endereco = Endereco.fromJson(jsonDoEndereco);
+        assertEquals("UF:", "PB", endereco.getUf());
+        assertEquals("Cidade:", "Patos", endereco.getCidade());
+        assertEquals("Bairro:", "Centro", endereco.getBairro());
+        assertEquals("Logradouro:", "Rua 1", endereco.getLogradouro());
+    }
+
+//    @Test
+//    @Config(manifest = Config.NONE)
+    public void deveriaLancarExcecaoAoConverterJsonParaEndereco() {
+        thrown.expect(EnderecoLoadException.class);
+        thrown.expectMessage("Erro ao converter endereço.");
+
+        String jsonDoEndereco = "";
+        Endereco endereco = Endereco.fromJson(jsonDoEndereco);
     }
 
 }
