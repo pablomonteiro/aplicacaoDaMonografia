293673453ebc42772ce5526915ba095d24240c39
pablomonteiro <pablomf84@gmail.com>
2016-03-01 12:50:20 -0300

    Ajustando teste para inclusao de novo usuario

--- app/src/androidTest/java/com/aplicacaodamonografia/view/FormularioActivityTest.java
+++ app/src/androidTest/java/com/aplicacaodamonografia/view/FormularioActivityTest.java
@@ -38,14 +38,14 @@ public class FormularioActivityTest {
         assertPreencimentoDoEndereco();
     }
 
-//    @Test
+    @Test
     public void deveriaIncluirNovoUsuario() {
         onView(withId(R.id.nome)).perform(typeText("Usuario 3"), closeSoftKeyboard());
         onView(withId(R.id.telefone)).perform(typeText("999998855"), closeSoftKeyboard());
         onView(withId(R.id.cep)).perform(typeText("60830005"), closeSoftKeyboard());
         onView(withId(R.id.btn_pesquisar)).perform(click());
         assertPreencimentoDoEndereco();
-//        onView(withId(R.id.btn_confirmar)).perform(click());
+        onView(withId(R.id.btn_confirmar)).perform(click());
 //        onView(isAssignableFrom(Toolbar.class))
 //                .check(matches(Util.withToolbarTitle(
 //                        Is.<CharSequence>is("Inclusão de Usuário"))));
